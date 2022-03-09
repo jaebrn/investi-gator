ebbf9c547cd8499f86ae7a1baa15b775b7b2e09f
jaebrn <80350744+jaebrn@users.noreply.github.com>
2022-03-09 14:37:40 -0500

    Functioning Lobby
    Doors to all rooms are functioning, inventory maintained, tags added to denote leading scene.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    /* to create a functional door:
+     * create an obj (door) with this script attached
+     * apply the tag correlating to the scene it should lead to
+     * done :D
+     */
+
+    private string doorTag;
+
+    private void Awake()
+    {
+        doorTag = gameObject.tag;
+    }
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene("Kitchen", LoadSceneMode.Single);
+            SceneManager.LoadScene(doorTag, LoadSceneMode.Single);            
         }
     }
 }
