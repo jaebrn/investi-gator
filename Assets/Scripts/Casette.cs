1e4c51524b48d2aec1dce06efa8f373d8a5d69c6
jaebrn <80350744+jaebrn@users.noreply.github.com>
2022-03-11 09:48:07 -0500

    Cassette fix

--- Assets/Scripts/Casette.cs
+++ Assets/Scripts/Casette.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class Casette : MonoBehaviour
 {
-    AudioSource audioSource;
+    public AudioSource audioSource;
+    public AudioClip[] audioClips; 
+
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -22,7 +24,38 @@ public class Casette : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            StartCoroutine(playAudioSequentially());
+        }
+    }
+
+    IEnumerator playAudioSequentially()
+    {
+        yield return null;
+
+        for(int i = 0; i<audioClips.Length; i++)
+        {
+            if(i == 0 || i == 1)
+            {
+                audioSource.pitch = 1.2f;
+            }else if(i == 2)
+            {
+                audioSource.pitch = 1.4f;
+            }
+            else
+            {
+                audioSource.pitch = 1.5f;
+            }
+            
+            audioSource.clip = audioClips[i];
             audioSource.Play();
+            print("playing");
+            print(i);
+
+            while (audioSource.isPlaying)
+            {
+                yield return null;
+            }
+
         }
     }
 }
